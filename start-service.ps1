0f1e21cf432d841926ae359c2920dc5ef39e996e
Yahaga <59613584+pzohaycuoi@users.noreply.github.com>
2021-11-10 15:47:39 +0700

    Update start-service.ps1

--- start-service.ps1
+++ start-service.ps1
@@ -1,28 +1,44 @@
 param (
     [Parameter()]
     [String[]]
-    $Service
+    $Services
 )
 
 # Loop through the service's name array, if service is not running then start the service
-foreach ($service in $Service) {
+foreach ($service in $Services) {
+
   for ($i = 0; $i -lt 3; $i++) {
+
     $getServiceStatus = Get-Service | Where-Object { $_.name -eq $service }
     if ($getServiceStatus.Status -eq "Running") {
+
       Continue
+
     }
+
     else {
+
       Start-Service -Name $service
       Start-Sleep -Seconds 2
+
     }
+
   }
+
 }
 
-foreach ($service in $Service) {
+
+foreach ($service in $Services) {
+
   $getStatAfterStart = Get-Service | Where-Object {$_.name -eq $service}
   if ($getStatAfterStart.Status -eq "Running") {
+
     Write-Output "[Succeed] Service $($getStatAfterStart.DisplayName) is running"
+
   } else {
+
     Write-Output "[Error] Service $($getStatAfterStart.DisplayName) is not running"
+
   }
+  
 }
